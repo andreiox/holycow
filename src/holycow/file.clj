d48a75d0bcb8438b201eb40d0458f94ce4166d8d
Andre Macedo <contato@andremacedo.dev>
2023-05-22 19:10:31 -0300

    test: words file to 5 letter words file

--- src/holycow/file.clj
+++ src/holycow/file.clj
@@ -1,3 +1,15 @@
-(ns holycow.file)
+(ns holycow.file
+  (:require [clojure.string :as string]))
 
-(println "Hello world")
+;get words from file
+(def words
+  (-> "resources/test-palavras"
+      slurp
+      string/trim
+      (string/split #"\n")))
+
+;write file with 5 letter words
+(->> words
+     (filter #(= 5 (count %)))
+     (string/join "\n")
+     (spit "resources/test-cinco-letras"))
