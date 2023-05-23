e6b2b3ef3ab14d8e300f6a62a866a09df3b97f0d
Andre Macedo <contato@andremacedo.dev>
2023-05-23 00:35:26 -0300

    feat: add ranks

--- src/holycow/file.clj
+++ src/holycow/file.clj
@@ -1,10 +1,11 @@
 (ns holycow.file
   (:require [clojure.string :as string]))
 
-(def words-path "resources/test-palavras")
-(def five-letter-words-path "resources/test-cinco-letras")
+(def words-path "resources/palavras")
+(def five-letter-words-path "resources/cinco-letras")
 
-(defn deaccent [str]
+(defn deaccent
+  [str]
   (java.text.Normalizer/normalize str java.text.Normalizer$Form/NFD))
 
 (defn read-words-file
@@ -27,10 +28,7 @@
 
 (defn add-occurrence
   ([letter map]
-   (let [occurrences (get map letter)]
-     (if occurrences
-       (assoc map letter (+ occurrences 1))
-       (assoc map letter 1))))
+   (add-occurrence letter 1 map))
   ([letter occurrences map]
    (let [sum-occurrences (get map letter)]
      (if sum-occurrences
@@ -48,4 +46,27 @@
 (def occurrences
   (reduce sum-occurrences {} (map letter-occurrences five-letter-words)))
 
-(println occurrences)
+(defn gen-score
+  [word occurrences]
+  {:word word
+   :score (reduce #(+ %1 (get occurrences %2)) 0 (re-seq #"." word))})
+
+(def ranks
+  (->> five-letter-words
+       (map #(gen-score % occurrences))
+       (sort-by :score)
+       reverse))
+
+(defn build-pattern-contains-distinct-chars
+  [word]
+  (re-pattern (str ".*(" (string/join "|" (distinct word)) ").*")))
+
+(def strongest-pair
+  (let [strongest-batman (first ranks)
+        robin-pattern (build-pattern-contains-distinct-chars (:word strongest-batman))
+        strongest-robin (first (filter (fn [word] (not (re-matches robin-pattern (:word word)))) ranks))]
+    {:first strongest-batman
+     :second strongest-robin
+     :score (+ (:score strongest-batman) (:score strongest-robin))}))
+
+strongest-pair
